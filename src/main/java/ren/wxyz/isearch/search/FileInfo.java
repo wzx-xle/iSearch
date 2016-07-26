7e1ab0d101575537aa29eeea8fbc3a571fb80ca5
wxyz <hyhjwzx@126.com>
2016-07-26 20:55:28 +0800

    重新定义存储文件信息，新增扩展名

--- src/main/java/ren/wxyz/isearch/search/FileInfo.java
+++ src/main/java/ren/wxyz/isearch/search/FileInfo.java
@@ -4,14 +4,14 @@ import lombok.Getter;
 import lombok.Setter;
 
 /**
- * 建索字段信息
+ * 文件的信息
  *
  * @author wxyz
  * @since 0.0.2
  */
 @Getter
 @Setter
-public class IndexFeildInfo {
+public class FileInfo {
 
     /**
      * 本地路径，使用据对路径并包括文件名
@@ -22,6 +22,11 @@ public class IndexFeildInfo {
      * 本地文件的Hash值，这里采用sha-256
      */
     private String hash;
+
+    /**
+     * 文件的扩展名
+     */
+    private String extName;
 
     /**
      * 本地文件的内容
